c05917b3bdb6b0809aee93d8f8558ef13424e529
viniciusalopes <suporte@viniciusalopes.com.br>
2020-09-28 17:58:07 -0300

    Conexão com o banco de dados (SINGLETON).

---
+++ aps/ApsCamadasMiniatura/src/br/com/vinicius/dal/BDConn.java
@@ -0,0 +1,84 @@
+/*
+ *  ----------------------------------------------------------------------------------------------->
+ *  Licença    : MIT - Copyright 2019 Viniciusalopes (Vovolinux) <suporte@vovolinux.com.br>
+ *  Criado em  : 28/09/2020 17:55:47 
+ *  Instituição: FACULDADE SENAI FATESG
+ *  Curso      : Análise e Desenvolvimento de sistemas - Módulo 3 - 2020/2
+ *  Disciplina : Arquitetura e Projeto de Software
+ *  Aluno      : Vinicius Araujo Lopes
+ *  Projeto    : ARQUITETURA EM CAMADAS
+ *  Exercício  : Cadastro de Miniatura em camadas
+ *  ------------------------------------------------------------------------------------------------
+ *  Conexão com o banco de dados (SINGLETON).
+ *  -----------------------------------------------------------------------------------------------| 
+ */
+package br.com.vinicius.dal;
+
+import br.com.vinicius.util.EnumStatus;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+/**
+ *
+ * @author vovostudio
+ */
+public class BDConn {
+
+    //--- ATRIBUTOS ------------------------------------------------------------------------------->
+    //
+    private static BDConn bdConn = null;
+    private static BDConfig config = null;
+    private static String url = "";
+    private static Properties props = null;
+    private Connection connection = null;
+
+    //--- FIM ATRIBUTOS ---------------------------------------------------------------------------|
+    //
+    //--- CONSTRUTORES ---------------------------------------------------------------------------->
+    //
+    private BDConn() {
+        config = BDConfig.getInstance();
+    }
+
+    //--- FIM CONSTRUTORES ------------------------------------------------------------------------|
+    //
+    //--- GET ------------------------------------------------------------------------------------->
+    //
+    public static BDConn getInstance() throws Exception {
+        try {
+            if (bdConn == null) {
+                bdConn = new BDConn();
+                config = BDConfig.getInstance();
+            }
+            url = "jdbc:postgresql:" + config.getServer() + ":" + config.getPort() + "/" + config.getDatabase();
+            props = new Properties();
+            props.setProperty("user", config.getUser());
+            props.setProperty("password", config.getPassword());
+
+            return bdConn;
+        } catch (Exception e) {
+            throw new Exception("Não foi possível localizar o banco de dados!\n" + e.getLocalizedMessage());
+        }
+    }
+
+    public Connection getConnection() throws Exception {
+        try {
+            Class.forName(config.getDriver());
+            connection = DriverManager.getConnection(url, props);
+            return connection;
+        } catch (Exception e) {
+            throw new Exception("Não foi possível obter a conexão com o banco de dados!\n" + e.getLocalizedMessage());
+        }
+    }
+
+    public EnumStatus getStatus() {
+        if (connection == null) {
+            return EnumStatus.INATIVA;
+        }
+        return EnumStatus.ATIVA;
+    }
+
+    //--- FIM GET ---------------------------------------------------------------------------------|
+    //
+}
---
+++ aps/ApsCamadasMiniatura/src/br/com/vinicius/util/EnumStatus.java
@@ -0,0 +1,27 @@
+/*
+ *  ----------------------------------------------------------------------------------------------->
+ *  Licença    : MIT - Copyright 2019 Viniciusalopes (Vovolinux) <suporte@vovolinux.com.br>
+ *  Criado em  : 28/09/2020 17:57:13 
+ *  Instituição: FACULDADE SENAI FATESG
+ *  Curso      : Análise e Desenvolvimento de sistemas - Módulo 3 - 2020/2
+ *  Disciplina : Arquitetura e Projeto de Software
+ *  Aluno      : Vinicius Araujo Lopes
+ *  Projeto    : ARQUITETURA EM CAMADAS
+ *  Exercício  : Cadastro de Miniatura em camadas
+ *  ------------------------------------------------------------------------------------------------
+ *  
+ *  -----------------------------------------------------------------------------------------------| 
+ */
+package br.com.vinicius.util;
+
+/**
+ *
+ * @author vovostudio
+ */
+public enum EnumStatus {
+    TODOS,
+    ATIVO,
+    ATIVA,
+    INATIVO,
+    INATIVA
+}
