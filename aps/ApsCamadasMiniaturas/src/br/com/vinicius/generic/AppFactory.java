60df635b5aa438b099df45d75948f8a5a5a29742
viniciusalopes <suporte@viniciusalopes.com.br>
2020-10-13 07:19:22 -0300

    generic

---
+++ aps/ApsCamadasMiniaturas/src/br/com/vinicius/generic/AppFactory.java
@@ -0,0 +1,92 @@
+/*
+ *  ----------------------------------------------------------------------------------------------->
+ *  Licença    : MIT - Copyright 2019 Viniciusalopes (Vovolinux) <suporte@vovolinux.com.br>
+ *  Criado em  : 11/10/2020 12:08:06 
+ *  Instituição: FACULDADE SENAI FATESG
+ *  Curso      : Análise e Desenvolvimento de sistemas - Módulo 3 - 2020/2
+ *  Disciplina : PP - Padrões de Projeto
+ *  Aluno      : Vinicius Araujo Lopes
+ *  Projeto    : SINGLETON / DECORATOR / TEMPLATE / FACTORY
+ *  Exercício  : Colaboradores de uma empresa
+ *  ------------------------------------------------------------------------------------------------
+ *  Propósito do arquivo.
+ *  -----------------------------------------------------------------------------------------------| 
+ */
+package br.com.vinicius.generic;
+
+import static br.com.vinicius.generic.AppDesktopMensagem.mensagemEscolher;
+import static br.com.vinicius.generic.Factory.getDal;
+import javax.swing.JTable;
+
+/**
+ *
+ * @author vovostudio
+ */
+public class AppFactory {
+
+    //--- JDialog --------------------------------------------------------------------------------->
+    //
+    /**
+     * Exibe um modal para inclusão ou edição de cadastro simples.
+     *
+     * @param object Objeto do cadastro.
+     * @param friendlyName Nome amigável da entidade.
+     * @param modal JDialog que implemente a interface AppIModal.
+     * @throws Exception
+     */
+    public static void getModal(Object object, String friendlyName, AppIModal modal) throws Exception {
+        modal.setObject(object);
+        modal.setFriendlyName(friendlyName);
+        modal.setVisible(true);
+    }
+
+    //--- FIM JDialog -----------------------------------------------------------------------------|
+    //
+    //--- JTable ---------------------------------------------------------------------------------->
+    //
+    /**
+     * Obtém o ID de um cadastro a partir a linha selecionada de um JTable, quando a coluna que
+     * contém o ID do objeto é 0 (zero).
+     *
+     * @param jTable JTable que se deseja obter o ID selecionado.
+     * @return
+     * @throws Exception
+     */
+    public static int getSelectedId(JTable jTable) throws Exception {
+        return getSelectedId(jTable, 0);
+    }
+
+    /**
+     * Obtém o ID de um cadastro a partir a linha selecionada de um JTable.
+     *
+     * @param jTable JTable que se deseja obter o ID selecionado.
+     * @param indexColumnId Índice da coluna que tem a informação do ID do objeto.
+     * @return
+     * @throws Exception
+     */
+    public static int getSelectedId(JTable jTable, int indexColumnId) throws Exception {
+        return Integer.parseInt(jTable.getValueAt(jTable.getSelectedRow(), indexColumnId).toString());
+    }
+
+    /**
+     * Executa o método delete da camada DAL de um objeto listado no JTable, a partir da linha
+     * selecionada.<br>
+     * Exibe uma mensagem de confirmação antes da deleção.
+     *
+     * @param jTable JTable que está exibindo a coleção de dados.
+     * @throws Exception
+     */
+    public static void deleteSelected(JTable jTable) throws Exception {
+        if (jTable.getSelectedRow() > -1) {
+            int id = getSelectedId(jTable);
+            String[] opcoes = new String[]{"Não... vai que, né!", "Sim"};
+            if (mensagemEscolher("Confirma a exclusão do cadastro [" + id + "] ? ", opcoes) > 0) {
+                Object obj = getDal(jTable.getAccessibleContext().getAccessibleName());
+                obj.getClass().getMethod("delete", int.class).invoke(obj, id);
+            }
+        }
+    }
+
+    //--- FIM JTable ------------------------------------------------------------------------------|
+    //
+}
---
+++ aps/ApsCamadasMiniaturas/src/br/com/vinicius/generic/Factory.java
@@ -0,0 +1,64 @@
+/*
+ *  ----------------------------------------------------------------------------------------------->
+ *  Licença    : MIT - Copyright 2019 Viniciusalopes (Vovolinux) <suporte@vovolinux.com.br>
+ *  Criado em  : 11/10/2020 12:00:03 
+ *  Instituição: FACULDADE SENAI FATESG
+ *  Curso      : Análise e Desenvolvimento de sistemas - Módulo 3 - 2020/2
+ *  Disciplina : PP - Padrões de Projeto
+ *  Aluno      : Vinicius Araujo Lopes
+ *  Projeto    : SINGLETON / DECORATOR / TEMPLATE / FACTORY
+ *  Exercício  : Colaboradores de uma empresa
+ *  ------------------------------------------------------------------------------------------------
+ *  Fábrica de objetos e classes, e chamadas a métodos.
+ *  -----------------------------------------------------------------------------------------------| 
+ */
+package br.com.vinicius.generic;
+
+/**
+ *
+ * @author vovostudio
+ */
+public abstract class Factory {
+
+    //--- ATRIBUTOS ------------------------------------------------------------------------------->
+    //
+    public static String domain = "br.com.vinicius.";
+
+    //--- FIM ATRIBUTOS ---------------------------------------------------------------------------|
+    //
+    //--- CONSTRUTORES ---------------------------------------------------------------------------->
+    public static Object getNewInstance(Class classe) throws Exception {
+        return classe.getConstructor().newInstance();
+    }
+
+    public static Object getDal(String className) throws Exception {
+        return getNewInstance(getClassFromPackage("dal", "Dal" + className));
+    }
+
+    //
+    //--- FIM CONSTRUTORES ------------------------------------------------------------------------|
+    //
+    //--- GET ------------------------------------------------------------------------------------->
+    //
+    public static Class getClassFromPackage(String packageName, String className) throws Exception {
+        return Class.forName(domain + packageName + "." + className);
+    }
+
+    public static Class getBll(String className) throws Exception {
+        return getClassFromPackage("bll", "Bll" + className);
+    }
+
+    public static int getId(Object object, String methodName) throws Exception {
+        return (int) object.getClass().getMethod(methodName).invoke(object);
+    }
+
+    public static Object invoke(Object object, String methodName) throws Exception {
+        return object.getClass().getMethod(methodName).invoke(object);
+    }
+
+    public static Object invoke(Object object, String methodName, Class argClass, Object arg) throws Exception {
+        return object.getClass().getMethod(methodName, argClass).invoke(object, arg);
+    }
+    //--- FIM GET ---------------------------------------------------------------------------------|
+    //
+}
